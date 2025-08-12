c4b30109d4a3814a236941d600e4a618537a67b9
Piyush Pal <121439075+piyushirish@users.noreply.github.com>
2025-08-13 02:22:38 +0530

    Update Exploratory Data Analysis.sql

--- Exploratory Data Analysis.sql
+++ Exploratory Data Analysis.sql
@@ -105,52 +105,6 @@ ORDER BY 2 DESC;
 
 
 
--- --------------------------------------------------------------------------------------------------------------
-
--- Earlier we looked at Companies with the most Layoffs. Now let's look at that per year.
--- I want to look at 
-
-WITH Company_Year AS 
-(
-  SELECT company, YEAR(date) AS years, SUM(total_laid_off) AS total_laid_off
-  FROM layoffs_staging2
-  GROUP BY company, YEAR(date)
-)
-, Company_Year_Rank AS (
-  SELECT company, years, total_laid_off, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
-  FROM Company_Year
-)
-SELECT company, years, total_laid_off, ranking
-FROM Company_Year_Rank
-WHERE ranking <= 3
-AND years IS NOT NULL
-ORDER BY years ASC, total_laid_off DESC;
-
-
-
-
--- Rolling Total of Layoffs Per Month
-SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
-FROM layoffs_staging2
-GROUP BY dates
-ORDER BY dates ASC;
-
--- now use it in a CTE so we can query off of it
-WITH DATE_CTE AS 
-(
-SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
-FROM layoffs_staging2
-GROUP BY dates
-ORDER BY dates ASC
-)
-SELECT dates, SUM(total_laid_off) OVER (ORDER BY dates ASC) as rolling_total_layoffs
-FROM DATE_CTE
-ORDER BY dates ASC;
-
-
-
-
-
 
 
 
